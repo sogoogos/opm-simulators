7a708d852f6c76cdeb9bf3bd28d4cbf3eeb56993
Andreas Lauser <and@poware.org>
2019-03-27 20:04:22 +0100

    ebos: set the exponent for the tolerance scaling factor do 0.5
    i.e., the acceptable error is scaled by the square root instead of the
    cube root of the pore volume.

--- ebos/eclproblem.hh
+++ ebos/eclproblem.hh
@@ -264,7 +264,7 @@ SET_SCALAR_PROP(EclBaseProblem, EclNewtonSumTolerance, 1e-4);
 // value specified by the NewtonSumTolerance parameter is the "incorrect" mass per
 // timestep for an reservoir that exhibits 1 m^3 of pore volume. A reservoir with a total
 // pore volume of 10^3 m^3 will tolerate 10 times as much.
-SET_SCALAR_PROP(EclBaseProblem, EclNewtonSumToleranceExponent, 1.0/3.0);
+SET_SCALAR_PROP(EclBaseProblem, EclNewtonSumToleranceExponent, 1.0/2.0);
 
 // set number of Newton iterations where the volumetric residual is considered for
 // convergence
